56f219b289a4c89a27de1abc93b92a5638a383ac
ALICEOCHOA <57851091+ALICEOCHOA@users.noreply.github.com>
2021-02-09 14:16:17 -0500

    Category Seeder

--- larapp/database/seeders/DatabaseSeeder.php
+++ larapp/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use \App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(1000)->create();
+        $this->call([
+            UserSeeder::class,
+            categorySeeder::class,
+            //GameSeeder::class,
+        ]);
+
     }
 }
